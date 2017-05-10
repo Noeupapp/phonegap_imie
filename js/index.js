836e882690e28bf41a626dd44bf14d5467e7b34a
quentin <quentin@noeupapp.com>
2017-05-10 14:43:51 +0200

    Prise de photo

--- js/index.js
+++ js/index.js
@@ -6,26 +6,43 @@ $(document).ready(function(){
 
     $("#clickAccel").on("click",function(){
 
-        function accelerometerSuccess(acceleration) {
-                $("#accelResult").html("<ul>"+
-                    "<li> en X: "+acceleration.x+"</li>"+
-                    "<li> en Y: "+acceleration.y+"</li>"+
-                    "<li> en Z: "+acceleration.z+"</li>"+
-                    "</ul>")
-            }
+        console.log("navigator.accelerometer",navigator.accelerometer)
 
-            function accelerometerError() {
-                alert('onError!');
-            }
+        // function accelerometerSuccess(acceleration) {
+        //         $("#accelResult").html("<ul>"+
+        //             "<li> en X: "+acceleration.x+"</li>"+
+        //             "<li> en Y: "+acceleration.y+"</li>"+
+        //             "<li> en Z: "+acceleration.z+"</li>"+
+        //             "</ul>")
+        //     }
 
-            watchID = navigator.accelerometer.watchAcceleration(accelerometerSuccess,
-                                                       accelerometerError,
-                                                       {frequency:1000});
+        //     function accelerometerError() {
+        //         alert('onError!');
+        //     }
+
+        //     watchID = navigator.accelerometer.watchAcceleration(accelerometerSuccess,
+        //                                                accelerometerError,
+        //                                                {frequency:1000});
     })
 
 
     $("#clearWatch").on("click",function(){
         navigator.accelerometer.clearWatch(watchID);
+    })
+
+    $("#takePhoto").on("click",function(){
+            navigator.camera.getPicture(onSuccessCamera, onFailCamera, { quality: 25,correctOrientation:true,
+                destinationType: Camera.DestinationType.DATA_URL
+            });
+
+            function onSuccessCamera(imageData) {
+                var image = document.getElementById('ImageIn');
+                image.src = "data:image/jpeg;base64," + imageData;
+            }
+
+            function onFailCamera(message) {
+                alert('Failed because: ' + message);
+            }
     })
 
 
